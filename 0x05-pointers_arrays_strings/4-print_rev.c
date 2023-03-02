c730240fdc090ab0218b833c8e78668f08bc3998
Brian-Rono <b.rono.br@gmail.com>
2023-03-02 02:49:04 -0800

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,14 +6,14 @@
  */
 void print_rev(char *s)
 {
-	int len;
-
-	for (len = 0; s[len] != '\0'; len++)
-	{
-	}
-	for (len = len - 1; len >= 0; len--)
-	{
-		_putchar(s[len]);
-	}
-	_putchar('\n');
+  int len;
+  
+  for (len = 0; s[len] != '\0'; len++)
+    {
+    }
+  for (len = len - 1; len >= 0; len--)
+    {
+      _putchar(s[len]);
+    }
+  _putchar('\n');
 }
