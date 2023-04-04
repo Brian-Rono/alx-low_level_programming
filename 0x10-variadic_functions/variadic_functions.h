035d28befe35e48c6de9e0a741f5833f0f3b82fd
Brian-Rono <b.rono.br@gmail.com>
2023-04-04 11:27:12 -0700

    Variadic functions.

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,26 +1,19 @@
-#include "variadic_functions.h"
-#include <stdio.h>
+#ifndef VARFUNC
+#define VARFUNC
 #include <stdarg.h>
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 /**
- * sum_them_all - sum all arguements
- * @n: number of arguements
- * Return: 0 is n is 0 or return sum
+ * struct typ - Struct operators
+ *
+ * @c: Char
+ * @tp: The function associated
  */
-
-int sum_them_all(const unsigned int n, ...)
+typedef struct typ
 {
-	va_list numbers;
-	int sum;
-	unsigned int i;
-
-	if (n == 0)
-		return (0);
-	sum = 0;
-	va_start(numbers, n);
-	for (i = 0; i < n; i++)
-	{
-		sum += va_arg(numbers, int);
-	}
-	va_end(numbers);
-	return (sum);
-}
+	char *c;
+	void (*tp)(va_list var);
+} typ_t;
+#endif
